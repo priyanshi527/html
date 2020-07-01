1488862a8d7b71759b6d96f264471988544bad6d
ayush-rawat25 <rawatayush004@gmail.com>
2020-07-01 12:41:23 +0530

    PORTFOLIO

---
+++ Portfolio/contact.php
@@ -0,0 +1,18 @@
+<?php
+    $first_name =$_POST['first-name'];
+    $last_name =$_POST['last-name'];
+    $visitor_email = $_POST['email'];
+    $message=$_POST['message'];
+
+    $email_from ='priyanshi9b@gmail.com';
+    $email_subject = "Portfolio feedback";
+    $email_body = "User Name: $first_name.+ " " + $last_name.\n".
+                        "User Email: $visitor_email.\n".
+                             "User Message: $message.\n";
+
+    $to = "priyanshi9b@gmail.com";
+    $headers = "From: $email_from \r\n";
+    $headers .="Reply-To: $visitor_email \r\n";
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location: portfolio.html");
+?>                            
--- Portfolio/js/home.js
+++ Portfolio/js/home.js
@@ -2,6 +2,6 @@ alert('Please give your feedback!')
 function post() {
 if(window.confirm("Are you sure you want to submit?"))
 {
-	window.open("exit.html" , "Thanks for Visiting!")
+	window.open("Thanks for Visiting!")
 }
 }
